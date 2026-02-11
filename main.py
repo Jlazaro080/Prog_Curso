45c1ce6efc1bf7fcfa15f006e2efeca834ab08ac
Jose Luis Lazaro <j.lazaro@posgrado.cidesi.edu.mx>
2026-02-11 09:19:49 -0600

    Se agrego una multiplicacion de dos numeros

--- main.py
+++ main.py
@@ -26,12 +26,14 @@ def main():
     print("\n--- CALCULADORA ---")
     print("1. Suma")
     print("2. Resta")
+    print("3. Multiplicacion")
     
-    opcion = input("\nElige una opción (1 o 2): ")
+    opcion = input("\nElige una opción (1, 2 o 3): ")
     
-    if opcion in ["1", "2"]:
+    if opcion in ["1", "2","3"]:
         num1 = float(input("Ingresa el primer número: "))
         num2 = float(input("Ingresa el segundo número: "))
+       
         
         if opcion == "1":
             resultado = suma(num1, num2)
@@ -39,6 +41,9 @@ def main():
         elif opcion == "2":
             resultado = resta(num1, num2)
             print(f"\nResultado: {num1} - {num2} = {resultado}")
+        elif opcion == "3":
+            resultado = num1 * num2
+            print(f"\nResultado: {num1} * {num2} = {resultado}")
     else:
         print("\nOpción no válida.")
     
